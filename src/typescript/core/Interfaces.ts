13f6f76dd544e1832f62bb54060c0b40fbdd0682
Alvin Joseph Lagutan <ajlagutan@outlook.com>
2025-04-23 04:57:33 +0800

    Add scene constructor

---
+++ src/typescript/core/Interfaces.ts
@@ -0,0 +1,13 @@
+import { SceneBase } from "./SceneBase";
+/**
+ * The constructor of {@link SceneBase} class.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @returns SceneBase
+ */
+export interface SceneConstructor {
+  new (): SceneBase;
+}
