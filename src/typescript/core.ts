bbc7a2264bce82ec669aa4b614662dc94a672490
Alvin Joseph Lagutan <ajlagutan@outlook.com>
2025-04-25 19:58:32 +0800

    Update core files

--- src/typescript/core.ts
+++ src/typescript/core.ts
@@ -2,5 +2,7 @@ export * from "./core/geometry/Geometry";
 export * from "./core/geometry/Vectors";
 
 export * from "./core/Inputs";
+export * from "./core/Interfaces";
 export * from "./core/SceneBase";
 export * from "./core/SceneManager";
+export * from "./core/SceneRecorder";
