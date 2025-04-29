1d4119cf98f88162747d59f91dc37df359046ae3
Alvin Joseph Lagutan <ajlagutan@outlook.com>
2025-04-30 05:35:18 +0800

    Add scenes common

---
+++ src/typescript/scenes/_common.ts
@@ -0,0 +1,15 @@
+/**
+ * The update modes of particle scenes.
+ *
+ *
+ *
+ *
+ *
+ */
+export type ParticleSceneUpdateMode =
+  | "colorAlpha"
+  | "colorPalette"
+  | "particleCount"
+  | "particleVelocity"
+  | "particleRadius"
+  | "particleSides";
