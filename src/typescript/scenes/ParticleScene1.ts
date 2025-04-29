72f65a4b1b2fc7ffe696a9ccf31f66e19d17d2ba
Alvin Joseph Lagutan <ajlagutan@outlook.com>
2025-04-30 07:26:23 +0800

    Update particle scene 1

--- src/typescript/scenes/ParticleScene1.ts
+++ src/typescript/scenes/ParticleScene1.ts
@@ -1,14 +1,14 @@
 import * as lil from "lil-gui";
-import { SceneBase } from "../core";
-import { Color, Particle } from "../objects";
+import { SceneBase, Vector3 } from "../core";
+import { Color, PolygonalParticle } from "../objects";
 import {
   chris_courses,
   clamp,
   distance,
   randomFloat,
-  randomInt,
   randomNonZero,
 } from "../utils";
+import { ParticleSceneUpdateMode } from "./_common";
 /**
  * Particle scene #1
  *
@@ -19,155 +19,191 @@ import {
  * @class
  */
 export class ParticleScene1 extends SceneBase {
-  private _collisions: boolean = true;
-  private _count: number = 50;
-  private _initialized: boolean = false;
-  private _particles: Array<Particle> = [];
-  private _particlesColor: Array<Color> = [];
-  private _radius: number = 32;
-  private _updatingParticleArray: boolean = false;
-  private _updatingParticleArrayLimit: number = 100;
-  /**
-   * Gets or sets if the particle can pass through one another or not.
-   * 
-   * 
-   * 
-   * 
-   * 
-   * @returns boolean
-   */
-  public get collisions(): boolean {
-    return this._collisions;
-  }
-  /**
-   * @param value The collision switch.
-   */
-  public set collisions(value: boolean) {
-    this._collisions = value;
-  }
-  /**
-   * Gets or sets the particle count.
-   *
-   *
-   *
-   *
-   *
-   * @returns number
-   */
-  public get count(): number {
-    return this._count;
-  }
-  /**
-   * @param value The particle count.
-   */
-  public set count(value: number) {
-    this._count = value;
-    this.updateParticleArray();
-  }
-  /**
-   * Gets or sets the particle radius.
-   *
-   *
-   *
-   *
-   *
-   * @returns number
-   */
-  public get radius(): number {
-    return this._radius;
-  }
-  /**
-   * @param value The radius of the particles.
-   */
-  public set radius(value: number) {
-    this._radius = value;
-    this._particles.forEach(function (p: Particle) {
-      p.radius = value;
-    });
-  }
+  private _particles: Array<PolygonalParticle> = [];
+  private _updatingParticles: boolean = false;
 
   public controller(gui: lil.GUI): void {
-    gui.add(this, "collisions").name("enable collisions");
-    gui.add(this, "count", 1, 1000, 1);
-    gui.add(this, "radius", 10, 100, 1);
+    gui.addColor(Options.Scene, "background");
+    gui.addColor(Options.Scene, "foreground");
+
+    const particles = gui.addFolder("particles");
+    const particleCount = new Vector3(5, 100, 1);
+    const particleRadius = new Vector3(10, 60, 1);
+    const particleSides = new Vector3(3, 9, 1);
+    const particleDistance = new Vector3(50, 1000, 1);
+    const particleVelocity = new Vector3(100, 1000, 1);
+
+    particles
+      .add(
+        Options.Particles,
+        "count",
+        particleCount.x,
+        particleCount.y,
+        particleCount.z
+      )
+      .onFinishChange(this.updateParticles.bind(this, "particleCount"));
+
+    particles
+      .add(
+        Options.Particles,
+        "radius",
+        particleRadius.x,
+        particleRadius.y,
+        particleRadius.z
+      )
+      .onChange(this.updateParticles.bind(this, "particleRadius"));
+
+    particles
+      .add(
+        Options.Particles,
+        "sides",
+        particleSides.x,
+        particleSides.y,
+        particleSides.z
+      )
+      .onChange(this.updateParticles.bind(this, "particleSides"));
+
+    particles
+      .add(
+        Options.Particles,
+        "maxDistance",
+        particleDistance.x,
+        particleDistance.y,
+        particleDistance.z
+      )
+      .name("max. distance");
+
+    particles
+      .add(
+        Options.Particles,
+        "maxVelocity",
+        particleVelocity.x,
+        particleVelocity.y,
+        particleVelocity.z
+      )
+      .onFinishChange(this.updateParticles.bind(this, "particleVelocity"))
+      .name("max. velocity");
   }
 
   public initialize(): void {
-    if (!this._initialized) {
-      this.updateParticleArray();
-      this._initialized = true;
-    }
+    this.updateParticles("particleCount");
   }
 
   public render(context: CanvasRenderingContext2D): void {
-    context.clearRect(0, 0, this.width, this.height);
     context.save();
-    for (
-      let i = 0;
-      i < this._particles.length && i < this._particlesColor.length;
-      i++
-    ) {
-      const p = this._particles[i];
-      const pc = this._particlesColor[i];
-      context.beginPath();
-      context.arc(p.x, p.y, p.radius, 0, Math.PI * 2);
-      context.closePath();
-      context.fillStyle = pc.toString();
-      context.fill();
-      context.stroke();
-    }
+    context.fillStyle = Options.Scene.background;
+    context.fillRect(0, 0, this.width, this.height);
     context.restore();
+    for (let p of this._particles) {
+      for (let q of this._particles) {
+        if (p === q) continue;
+        let totalDistance =
+          distance(p.x, p.y, q.x, q.y) - (p.radius + q.radius);
+        let totalAlpha = 1 - totalDistance / Options.Particles.maxDistance;
+        let color = Color.fromHtmlString(Options.Scene.foreground);
+        color.w = clamp(totalAlpha, 0, 1);
+        context.beginPath();
+        context.moveTo(p.x, p.y);
+        context.lineTo(q.x, q.y);
+        context.closePath();
+        context.save();
+        context.lineWidth = 3 * totalAlpha;
+        context.strokeStyle = color.toString();
+        context.stroke();
+        context.restore();
+      }
+      context.save();
+      context.fillStyle = Options.Scene.foreground;
+      context.fill(p.path);
+      context.restore();
+    }
   }
 
   public update(time: number): void {
-    if (this._updatingParticleArray) return;
-    for (let i = 0; i < this._count; i++) {
-      const pi = this._particles[i];
-      if (this._collisions) {
-        for (let j = 0; j < this._count; j++) {
-          const pj = this._particles[j];
-          if (pi === pj) continue;
-          if (distance(pi.x, pi.y, pj.x, pj.y) - (pi.radius + pj.radius) < 0) {
-            chris_courses.resolveCollision(pi, pj);
-          }
+    if (this._updatingParticles) {
+      return;
+    }
+    for (let p of this._particles) {
+      for (let q of this._particles) {
+        if (p === q) continue;
+        if (distance(p.x, p.y, q.x, q.y) - (p.radius + q.radius) < 0) {
+          chris_courses.resolveCollision(p, q);
         }
       }
-      if (pi.x < pi.radius || pi.x > this.width - pi.radius) {
-        pi.x = clamp(pi.x, pi.radius, this.width - pi.radius);
-        pi.velocity.x = -pi.velocity.x;
+      if (p.x < p.radius || p.x > this.width - p.radius) {
+        p.x = clamp(p.x, p.radius, this.width - p.radius);
+        p.velocity.x = -p.velocity.x;
       }
-      if (pi.y < pi.radius || pi.y > this.height - pi.radius) {
-        pi.y = clamp(pi.y, pi.radius, this.height - pi.radius);
-        pi.velocity.y = -pi.velocity.y;
+      if (p.y < p.radius || p.y > this.height - p.radius) {
+        p.y = clamp(p.y, p.radius, this.height - p.radius);
+        p.velocity.y = -p.velocity.y;
       }
-      pi.x += pi.velocity.x * time;
-      pi.y += pi.velocity.y * time;
+      p.x += p.velocity.x * time;
+      p.y += p.velocity.y * time;
     }
   }
 
-  private updateParticleArray(): void {
-    this._updatingParticleArray = true;
-    let diff = Math.abs(this._particles.length - this._count);
-    if (this._count > this._particles.length) {
-      for (let i = 0; i < diff; i++) {
-        const hue = randomFloat(0, 360);
-        const radius = this._radius;
-        let x = this.width / 2;
-        let y = this.height / 2;
-        const p = new Particle(x, y, radius);
-        const pc = Color.fromHsla(hue, 100, 50);
-        p.mass = 1;
-        p.velocity.x = randomNonZero(-50, 50);
-        p.velocity.y = randomNonZero(-50, 50);
-        this._particles.push(p);
-        this._particlesColor.push(pc);
+  private updateParticles(mode: ParticleSceneUpdateMode): void {
+    if (mode === "particleCount") {
+      this._updatingParticles = true;
+      const diff = Math.abs(Options.Particles.count - this._particles.length);
+      if (Options.Particles.count > this._particles.length) {
+        const color = Color.transparent;
+        const radius = Options.Particles.radius;
+        const sides = Options.Particles.sides;
+        const velocity = Options.Particles.maxVelocity;
+        for (let i = 0; i < diff; i++) {
+          const x = randomFloat(radius, this.width - radius);
+          const y = randomFloat(radius, this.height - radius);
+          const p = new PolygonalParticle(x, y, radius, sides, color);
+          p.mass = 1;
+          p.velocity.x = randomNonZero(-1, 2) * velocity;
+          p.velocity.y = randomNonZero(-1, 2) * velocity;
+          this._particles.push(p);
+        }
+      } else if (Options.Particles.count < this._particles.length) {
+        for (let i = 0; i < diff; i++) {
+          const p = this._particles.length - 1;
+          this._particles.splice(p, 1);
+        }
       }
-    } else if (this._count < this._particles.length) {
-      for (let i = 0; i < diff; i++) {
-        const p = randomInt(0, this._particles.length);
-        this._particles.splice(p, 1);
+    } else {
+      this._updatingParticles = true;
+      for (let p of this._particles) {
+        if (mode === "particleRadius") {
+          p.radius = Options.Particles.radius;
+          continue;
+        }
+        if (mode === "particleSides") {
+          p.sides = Options.Particles.sides;
+          continue;
+        }
+        if (mode === "particleVelocity") {
+          p.velocity.x = randomNonZero(-1, 2) * Options.Particles.maxVelocity;
+          p.velocity.y = randomNonZero(-1, 2) * Options.Particles.maxVelocity;
+          continue;
+        }
       }
     }
-    this._updatingParticleArray = false;
+    this._updatingParticles = false;
+  }
+}
+/**
+ * A namespace for the scene options.
+ *
+ * @namespace
+ */
+namespace Options {
+  export abstract class Particles {
+    public static count: number = 100;
+    public static maxDistance: number = 150;
+    public static maxVelocity: number = 300;
+    public static radius: number = 10;
+    public static sides: number = 6;
+  }
+
+  export abstract class Scene {
+    public static background: string = "#ffffff";
+    public static foreground: string = "#000000";
   }
 }
