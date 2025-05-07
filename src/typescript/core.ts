de64b042db7995b55d05bd4f20daef0eb3fcc6ed
Alvin Joseph Lagutan <ajlagutan@outlook.com>
2025-05-08 01:58:18 +0800

    Add inputs

--- src/typescript/core.ts
+++ src/typescript/core.ts
@@ -1,8 +1,10 @@
 export * from "./core/geometry/Geometry";
 export * from "./core/geometry/Vectors";
 
+export * from "./core/inputs/Keyboard";
+export * from "./core/inputs/Mouse";
+
 export * from "./core/Assets";
-export * from "./core/Inputs";
 export * from "./core/Interfaces";
 export * from "./core/SceneBase";
 export * from "./core/SceneManager";
