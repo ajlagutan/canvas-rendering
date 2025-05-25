b2e3d3eb30435eb3ee24f20de92e7652748e4c2a
AJ Lagutan <alvin.lagutan@ghl.com>
2025-05-25 10:42:21 +0800

    Add debug log extension function

--- src/typescript/utils.ts
+++ src/typescript/utils.ts
@@ -1,3 +1,7 @@
 export * from "./utils/math"
 export * from "./utils/physics"
 export * from "./utils/randomizer"
+
+export function debug(prototype: any, ...data: any[]): void {
+  console.debug(`[${prototype.constructor.name}]`, ...data);
+}
