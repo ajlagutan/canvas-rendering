1ce46cbcf5357b9b6d93e7d98e008d98c3751f93
AJ Lagutan <alvin.lagutan@ghl.com>
2025-05-25 10:41:56 +0800

    Remove interface code file

--- src/typescript/core.ts
+++ src/typescript/core.ts
@@ -5,7 +5,6 @@ export * from "./core/inputs/Keyboard";
 export * from "./core/inputs/Mouse";
 
 export * from "./core/Assets";
-export * from "./core/Interfaces";
 export * from "./core/SceneBase";
 export * from "./core/SceneManager";
 export * from "./core/SceneRecorder";
