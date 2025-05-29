125e0dab90482b700849d4e606fb0d3910319a1a
AJ Lagutan <alvin.lagutan@ghl.com>
2025-05-29 14:56:43 +0800

    Add clamp utility function

--- src/scripts/core/_utils.ts
+++ src/scripts/core/_utils.ts
@@ -37,3 +37,16 @@ export namespace logger {
     console.error(`[${this.name}]`, ...data);
   }
 }
+/**
+ * Clamps a value between the minimum and maximum value.
+ *
+ *
+ *
+ * @param value The current value to clamp.
+ * @param min The minimum value.
+ * @param max The maximum value.
+ * @returns number
+ */
+export function clamp(value: number, min: number, max: number): number {
+  return Math.min(max, Math.max(min, value));
+}
