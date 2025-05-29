3d1689514b9c0bafecf40d0342f8f675d57195b5
AJ Lagutan <alvin.lagutan@ghl.com>
2025-05-29 14:57:40 +0800

    Add rng utility functions

--- src/scripts/core/_utils.ts
+++ src/scripts/core/_utils.ts
@@ -38,6 +38,72 @@ export namespace logger {
   }
 }
 /**
+ * Utility functions for generating random numbers.
+ *
+ *
+ *
+ * @namespace
+ */
+export namespace rng {
+  /**
+   * Generates a random floating number between the minimum and maximum value.
+   *
+   *
+   *
+   * @param min The minimum value, inclusive.
+   * @param max The maximum value, inclusive.
+   * @returns number
+   */
+  export function float(min: number, max: number): number {
+    if (min >= max) {
+      throw new Error("Invalid range: min must be less than max");
+    }
+    return Math.random() * (max - min) + min;
+  }
+  /**
+   * Generates a random integer between the minimum (inclusive) and maximum (exclusive) value.
+   *
+   *
+   *
+   * @param min The minimum value, inclusive.
+   * @param max The maximum value, exclusive.
+   * @returns number
+   */
+  export function int(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    if (min >= max) {
+      throw new Error("Invalid range: min must be less than max");
+    }
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  /**
+   * Generates a random non-zero integer between
+   * the minimum (inclusive) and maximum (exlusive) value.
+   *
+   *
+   *
+   * @param min The minimum value, inclusive.
+   * @param max The maximum value, exclusive.
+   * @returns number
+   */
+  export function nonzero(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    if (min >= max) {
+      throw new Error("Invalid range: min must be less than max");
+    }
+    if (min <= 0 && max >= 0 && max - min === 1) {
+      throw new Error("Only possible result is zero, which is not allowed");
+    }
+    let num: number;
+    do {
+      num = Math.floor(Math.random() * (max - min)) + min;
+    } while (num === 0);
+    return num;
+  }
+}
+/**
  * Clamps a value between the minimum and maximum value.
  *
  *
